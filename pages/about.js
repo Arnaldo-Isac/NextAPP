eb4df89008d8f9f2b7520df433af6ff62272bb88
Arnaldo-Isac <manguene734@gmail.com>
2022-09-14 15:59:34 +0200

    textos

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 export default function About(){
     return <div>
-            <h1>Página ASobre nós</h1>
+            <h1>Página Sobre nós</h1>
             <p>Acerca da empresa ou instituição
                 voltar a página <Link href="/"><a>Principal</a> </Link></p>
         
