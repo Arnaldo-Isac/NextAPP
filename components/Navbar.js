9710a3207a4ee8281898e34665210ba3712b252a
Arnaldo-Isac <manguene734@gmail.com>
2022-09-16 14:35:59 +0200

    Adicionar Componentes Footer e Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+export default function Navbar(){
+    return <>
+        <ul>
+            <il>
+                <Link href="/"><a>
+                    Home
+                </a></Link>
+            </il>
+            <il>
+                <Link href="/"><a>
+                    /Home/
+                </a></Link>
+            </il>
+            <il>
+                <Link href="about"><a>
+                    About
+                </a></Link>
+            </il>            
+            <il>
+                <Link href="contacts"><a>
+                    Contactos
+                </a></Link>
+            </il>
+        </ul>
+    </>
+}
