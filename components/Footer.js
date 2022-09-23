30363f829fd6b0ae9029bc76c014d80861262272
Armaldo-isac <manguene734@gmail.com>
2022-09-23 17:25:26 +0200

    confrmarr

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,11 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer(){
     return (
-    <>  
-        <footer className='{styles.footer}'>Copyright &copy; 2022</footer>
-    </>)
+        <footer className={ styles.footer }>
+            <p>Copyright &copy; 2022</p>
+            <p>
+                //
+            </p>
+        </footer>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,20 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-function Home(){
-    return <div className='{styles.main}'>
+function Home() {
+    return <div className={styles.main}>
         <h1>Home</h1>
         <p>Verificado</p>
         <p>Aterroziznte </p>
         Visitar a página <Link href="/about"><a>Sobre nós</a></Link> na empresa ou ir a <a href="product">Produtos</a>
-        <br></br><Image src="/images/city.jpg" height="600px" width="400px" alt="New Sitle" />
-        
+        <br></br>
+        <Image 
+            src="/images/city.jpg" 
+            height="600px" 
+            width="400px" 
+            alt="New Sitle" 
+        />
+
     </div>
 }
 
