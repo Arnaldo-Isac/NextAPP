cdfd725e1b45875a61797e9bb0d4d321326baabb
Arnaldo-Isac <44202720+Arnaldo-Isac@users.noreply.github.com>
2025-03-19 16:34:00 +0200

    Update 1

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home() {
     return <div className={styles.main}>
         <h1>Home</h1>
         <p>Verificado</p>
-        <p>Aterroziznte </p>
+        
         Visitar a página <Link href="/about"><a>Sobre nós</a></Link> na empresa ou ir a <a href="product">Produtos</a>
         <br></br>
         <Image 
